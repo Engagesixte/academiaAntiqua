f7d061a6c8e49b67981c02b9d713bc42d1db3221
David <clerensdavid@gmail.com>
2026-04-13 18:42:50 +0200

    Premier Commit

---
+++ script.js
@@ -0,0 +1,20 @@
+document.getElementById("mythologie").onclick = () => {
+  window.location.href = "mythologie.html";
+};
+
+document.getElementById("culture").onclick = () => {
+  window.location.href = "culture.html";
+};
+
+document.getElementById("textes").onclick = () => {
+  window.location.href = "textes.html";
+};
+
+document.getElementById("grammaire").onclick = () => {
+  window.location.href = "grammaire.html";
+};
+
+document.getElementById("vocabulaire").onclick = () => {
+  window.location.href = "vocabulaire.html";
+};
+
