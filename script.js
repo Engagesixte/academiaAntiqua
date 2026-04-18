b72e1225c44f700aaf64e0a077a6a1d06025b260
David <clerensdavid@gmail.com>
2026-04-18 16:20:53 +0200

    Mise à jour du site

--- script.js
+++ script.js
@@ -1,20 +1,35 @@
-document.getElementById("mythologie").onclick = () => {
-  window.location.href = "./mythologie/mythologie.html";
+document.getElementById("mythologieGrecque").onclick = () => {
+  window.location.href = "mythologieGrecque/mythologie.html";
 };
 
-document.getElementById("culture").onclick = () => {
-  window.location.href = "culture.html";
+document.getElementById("mythologieScandinave").onclick = () => {
+  window.location.href = "mythologieScandinave/mythologieScandinave.html";
 };
 
-document.getElementById("textes").onclick = () => {
-  window.location.href = "textes.html";
+document.getElementById("mythologieMesopotamienne").onclick = () => {
+  window.location.href = "mythologieMesopotamienne/mythologieMesopotamienne.html";
 };
 
-document.getElementById("grammaire").onclick = () => {
-  window.location.href = "grammaire.html";
+document.getElementById("mythologieEgyptienne").onclick = () => {
+  window.location.href = "mythologieEgyptienne/mythologieEgyptienne.html";
 };
 
-document.getElementById("vocabulaire").onclick = () => {
-  window.location.href = "vocabulaire.html";
+document.getElementById("mythologieHindoue").onclick = () => {
+  window.location.href = "mythologieHindoue/mythologieHindoue.html";
 };
+
+document.getElementById("mythologieChinoise").onclick = () => {
+  window.location.href = "mythologieChinoise/mythologieChinoise.html";
+};
+
+document.getElementById("mythologieAmerindienne").onclick = () => {
+  window.location.href = "mythologieAmerindienne/mythologieAmerindienne.html";
+};
+
+document.getElementById("mythologieSubSaharienne").onclick = () => {
+  window.location.href = "mythologieSubSaharienne/mythologieSubSaharienne.html";
+};
+     
+     
+      
 
